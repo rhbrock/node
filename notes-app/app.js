26e5164e28743f7fcff304e4a0b990cddc7753ac
Roger H Brock <rogerhbrock@gmail.com>
2019-08-08 11:48:28 -0400

    Second Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,31 @@
-const add = require('./util.js')
+//ES6... Not yet supported by Node
+//import validator from 'validator';
 
-const sum = add(4, 2)
-
-console.log(sum)
-
-
+const validator = require('validator')
+const notes = require('./notes.js')
 
 
+const note = notes('This is a note')
+console.log(note)
+
+console.log(validator.isEmail('roger@example.com'))
+console.log(validator.isEmail('roger@example.'))
+console.log(validator.isEmail('rogerexample.com'))
+
+console.log(validator.isURL('https://something.com'))
 
 //old
+// const add = require('./util.js')node
+// const notes = require('./notes.js')
+
+// const sum = add(4, 2)
+
+// console.log(sum)
+
+// const note = notes('This is a note')
+// console.log(note)
+
+
 
 // const fs = require('fs')
 
---
+++ notes-app/notes.js
@@ -0,0 +1,9 @@
+console.log('You are in notes.js')
+
+//const note;
+
+const noteReturn = function (note) {
+    return note;
+}
+
+module.exports = noteReturn
