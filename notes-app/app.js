680d5364ce314c59a5b3c88bb4b89f992ae1e714
Roger H Brock <rogerhbrock@gmail.com>
2019-08-08 07:50:31 -0400

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,18 @@
+const add = require('./util.js')
+
+const sum = add(4, 2)
+
+console.log(sum)
+
+
+
+
+
+//old
+
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+
+// fs.appendFileSync('notes.txt', 'Newer Message')
+// console.log("Newer message added")
